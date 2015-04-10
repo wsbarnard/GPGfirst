9e23c4c0ec612f46b8161bc7f39ac932938a4741
wsbarnard <wsbarnard@wpi.edu>
2015-04-10 16:50:11 -0400

    more like greedy best first

--- gpg_lab3/Scripts/main3.py
+++ gpg_lab3/Scripts/main3.py
@@ -99,11 +99,11 @@ def poseCB(msg):
     global robotPosex
     global robotPosey
 
-    tempx = msg.pose.pose.position.x
-    tempy = msg.pose.pose.position.y
+    tempx = round(msg.pose.pose.position.x, 1)
+    tempy = round(msg.pose.pose.position.y, 1)
 
-    robotPosex = (tempx) - (tempx%0.2)
-    robotPosey = (tempy) - (tempy%0.2)
+    robotPosex = tempx - (tempx%0.2)
+    robotPosey = tempy - (tempy%0.2)
     print "Xpose: %f" % (robotPosex)
     print "Ypose: %f" % (robotPosey)
     
@@ -117,27 +117,30 @@ def goalPoseCB(msg):
     tempx = msg.pose.position.x
     tempy = msg.pose.position.y
 
-    goalPosex = (tempx) - (tempx%0.2)
-    goalPosey = (tempy) - (tempy%0.2)
+    goalPosex = round((tempx - (tempx%0.2)),1)
+    goalPosey = round((tempy - (tempy%0.2)),1)
     print "XgoalPose %f" % (goalPosex)
     print "YgoalPose %f" % (goalPosey)
 
 
-def calcHeuristic(point): #point was previously not a thing...but now we can hero any point
+def calcHeuristic(point): 
     global robotPosex
     global robotPosey
     global goalPosex
     global goalPosey
-    global hero 
 
     #DOUBLE CHECKTHIS was robotPosex and robotPosey
-    hero = abs(point.x - goalPosex) + abs(point.y - goalPosey)
-    print "Heuristic: %f" % (hero)
+    return abs(point.x - goalPosex) + abs(point.y - goalPosey)
+
 
 def coordHeuristic(pointX, pointY):
     global goalPosex
     global goalPoxey
-    return float((int(abs(pointX - goalPosex) + abs(pointY - goalPosey)) * 10)) / 10
+    # return float((int(abs(pointX - goalPosex) + abs(pointY - goalPosey)) * 10)) / 10
+    return abs(pointX - goalPosex) + abs(pointY - goalPosey)
+
+def twoPointHeuristic(node1, node2):
+    return abs(node1.x - node2.x) + abs(node1.y - node2.y)
 
 class Node:
     def __init__(self, x, y, h, chance): #removed neighbors.
@@ -145,44 +148,8 @@ class Node:
         self.y = y
         self.h = h
         self.chance = chance
+        self.parent = None
 
-
-def makeGraph():
-    global mapWidth
-    global mapData 
-    global mapList
-    global mapOrigin
-    global nodeList
-
-    nodeList = []
-    mapList = []
-
-    tempx = 0
-    tempy = 0
-    
-    for ind in range(len(mapData)):
-        # import pdb; pdb.set_trace()
-        tempx = int((((mapRes * ((ind % (mapWidth)))) + mapOrigin.position.x + 0.1)) * 10)
-        tempy = int((((mapRes * int((ind / mapWidth))) + mapOrigin.position.y + 0.1)) * 10)
-
-        xPlace = float(tempx) / 10
-        yPlace = float(tempy) / 10
-
-        h = coordHeuristic(xPlace, yPlace) + (mapData[ind]*100) #make it very difficult to go t\hrough obstacle 
-        N = Node(xPlace, yPlace, h, mapData[ind])
-        # print "%f, %f"%(N.x, N.y)
-        # print N.heuristic
-        # print N.chance
-        nodeList.append(N)
-        #print nodeList
-
-        i = 0
-        if (ind % mapWidth) == mapWidth - 1:
-            mapList.append(nodeList)
-            nodeList = []
-            i+1
-        N.self = (i*(mapWidth-1))+ ind
-        # print N.self
 
 def makeGraph2():
     global mapWidth
@@ -195,18 +162,18 @@ def makeGraph2():
     ytemp = 0
 
     for ind in range(len(mapData)):
-        xtemp = int((((mapRes * ((ind % (mapWidth)))) + mapOrigin.position.x)) * 10)
-        ytemp = int((((mapRes * ((ind / mapWidth))) + mapOrigin.position.y)) * 10)
+        xMapCoord = round(((mapRes * ((ind % (mapWidth)))) + mapOrigin.position.x), 1) + 0.1
+        yMapCoord = round(((mapRes * ((ind / mapWidth))) + mapOrigin.position.y), 1) + 0.1
 
         #print "temp %f, %f" %(xtemp, ytemp)
         
-        xMapCoord = float(xtemp) / 10
-        if xMapCoord > 0:
-            xMapCoord += 0.1
-        yMapCoord = float(ytemp) / 10
-        if yMapCoord > 0:
-            yMapCoord += 0.1
-        #print "legit %f, %f" %(xMapCoord, yMapCoord)
+        # xMapCoord = float(xtemp) / 10
+        # if xMapCoord > 0:
+        #     xMapCoord += 0.1
+        # yMapCoord = float(ytemp) / 10
+        # if yMapCoord > 0:
+        #     yMapCoord += 0.1
+        # print "legit %f, %f" %(xMapCoord, yMapCoord)
 
         h = coordHeuristic(xMapCoord, yMapCoord) + (mapData[ind]*100) #make it very difficult to go through obstacle
 
@@ -229,25 +196,74 @@ def aStar2(graph):
     visited = []
     pathList = []
     costSoFar = 0
-    currentNode = None
-    startx = int(robotPosex / mapWidth)
-    starty = int(robotPosey % mapWidth)
+    estCost = 0
 
-    currentNode = graph[starty][startx]
+    currentNode = isStart(graph)
+    createCell(robotPosex,robotPosey,"greenCells")
+
     frontier.append(currentNode)
-    costSoFar += currentNode.h
-    while frontier is not empty:
+    estCost = currentNode.h
+    while len(frontier) > 0:
         currentNode = findLowestCost(frontier)
      
         #if nothing works check tolerances
-        if(currentNode.x == goalPosex) and (currentNode.y == goalPosey):
-            return pathList
-
+        xdif = currentNode.x - goalPosex
+        ydif = currentNode.y - goalPosey
+        # if (xdif < 0.1 and xdif > -0.1 and ydif < 0.1 and ydif > -0.1):
+        #     return getPath(currentNode)
+        if((currentNode.x == goalPosex) and (currentNode.y == goalPosey)):
+            print "currentNode coords %f %f" % (currentNode.x, currentNode.y)
+            print "goal coords        %f %f" % (goalPosex, goalPosey)
+            return getPath(currentNode)
+           
         frontier.remove(currentNode)
         visited.append(currentNode)
 
-        getNeighbors(currentNode, graph)
-        
+        currentNeighbors = getNeighbors(currentNode, graph)
+
+        for neighbor in currentNeighbors:
+
+
+            addCell(neighbor.x,neighbor.y, "greenCells")
+            
+
+            if (neighbor in visited):
+                continue
+            tempCost = costSoFar + twoPointHeuristic(currentNode, neighbor)
+            aFuckingHugeNumber = 100000
+            tempCost += (neighbor.chance * aFuckingHugeNumber)
+            
+            if (neighbor not in frontier) or (tempCost < costSoFar):
+                neighbor.parent = currentNode
+                costSoFar = tempCost
+                estCost = costSoFar + calcHeuristic(neighbor)
+                if neighbor not in frontier:
+                    
+                    frontier.append(neighbor)
+                    print len(frontier)
+                    print len(visited)
+    print "A STAR DIDNT RETURN"
+
+def isStart(listofNodes):
+        global robotPosex
+        global robotPosey
+
+        for node in listofNodes:
+            xdif = round((node.x - robotPosex), 1)
+            ydif = round((node.y - robotPosey), 1)
+            print "%f %f" % (xdif, ydif)
+            # if (xdif < 0.1 and xdif > -0.1 and ydif < 0.1 and ydif > -0.1):
+            #     return node
+            if (xdif == 0 and ydif == 0):
+                print "this is a statement!!!!"
+                return node
+
+def getPath(current):
+    finalPath = [current]
+    while current.parent is not None:
+        finalPath.append(current.parent)
+        current = current.parent
+    return finalPath
   
 def findLowestCost(fList):
     lowNode = Node(0, 0, 10000, 0)
@@ -255,7 +271,7 @@ def findLowestCost(fList):
         if e.h < lowNode.h:
             lowNode = e
             
-    return e
+    return lowNode
 
 def getNeighbors(ofNode, ofGraph):
     nList = []
@@ -278,98 +294,20 @@ def isNeighbor(node1, node2):
     if ((diffx1 > 0.1) and (diffx1 < 0.3)) or ((diffx2 > 0.1) and (diffx2 < 0.3)) or ((diffx1 > -0.1) and (diffx1 < 0.1)) :
         if ((diffy1 > 0.1) and (diffy1 < 0.3)) or ((diffy2 > 0.1) and (diffy2 < 0.3)) or ((diffy1 > -0.1) and (diffy1 < 0.1)):
             return 1
+    # if (node1.x == node2.x and node1.y == node2.y):
+    #     return 0
+
+    # if ((node1.x == node2.x) and ((node1.y == (node2.y+0.2)) or (node1.y == (node2.y-0.2)))):
+    #     print "neighbor found"
+    #     return 1
+
+    # if ((node1.y == node2.y) and ((node1.x == (node2.x+0.2)) or (node1.x == (node2.x-0.2)))):
+    #     print "neighbor found"  
+    #     return 1
 
     #if (diffx1 > 0.1) and (diffx1 < 0.3) 
-    return 0
+    return 0 
 
-
-
-def aStar(graph):
-    global robotPosex
-    global robotPosey
-    global goalPosex
-    global goalPosey
-    global mapWidth
-
-    frontier = []
-    pathList = []
-    visited = []
-    costSoFar = 0
-    startNode = 0
-    startx = 0
-    starty = 0
-
-    print robotPosex
-    print robotPosey
-
-    for ind in range(mapWidth**2):
-        currentNode = graph[ind%mapWidth][ind/mapWidth]
-        if (currentNode.x == robotPosex) and (currentNode.y == robotPosey):
-            startNode = currentNode
-            rospy.sleep(.5)
-            startx = ind/mapWidth
-            rospy.sleep(.5)
-            starty = ind%mapWidth
-            # print "this happened, and then therapy"
-            break
-    # print "exited for loop"
-
-
-    # frontier.append(startNode)
-
-    #check heuristic, return one that has smallest total cost
-    #add to path
-    print "WTF A*"
-    createCell(startNode.x, startNode.y, "redCells")
-
-    # import pdb; pdb.set_trace()
-    while (startNode.x != goalPosex) or (startNode.y != goalPosey): #CHANGE THAT TO SOMETHNG LEGIT
-        print "dats what im talkin bout"
-        visited.append(startNode)
-        pathList.append(startNode)
-        cost1 = 10000000
-        cost2 = cost1
-        cost3 = cost1
-        cost4 = cost1
-
-        if (startx + 1 < mapWidth) and (graph[starty][startx+1] not in visited):
-            frontier.append(graph[starty][startx + 1])
-            cost1 = graph[starty][startx + 1].h
-        if (startx - 1 > 0) and (graph[starty][startx-1] not in visited):
-            frontier.append(graph[starty][startx - 1])
-            cost2 = graph[starty][startx - 1].h
-        if (starty - 1 > 0) and (graph[starty - 1][startx] not in visited):
-            frontier.append(graph[starty - 1][startx])
-            cost3 = graph[starty - 1][startx].h
-        if (starty + 1 < mapWidth) and (graph[starty +1][startx] not in visited):
-            frontier.append(graph[starty + 1][startx])
-            cost4 = graph[starty + 1][startx].h
-        costSoFar += min(cost1, cost2, cost3, cost4)
-        if costSoFar == cost1:
-            startx=startx + 1
-        elif costSoFar == cost2:
-            startx=startx - 1
-        elif costSoFar == cost3:
-            starty=starty - 1
-        elif costSoFar == cost4:
-            starty=starty + 1
-        # frontier.remove(startNode)
-        addCell(startNode.x, startNode.y, "redCells")
-        startNode = graph[starty][startx]
-        rospy.sleep(.3)
-        
-
-    print "check path"
-    rospy.sleep(.3)
-    createCell(robotPosex,robotPosey,"greenCells")
-    for ind in pathList:
-        rospy.sleep(.3)
-        addCell(ind.x,ind.y,"greenCells")
-        print ind.self
-
-
-
-      
 
 if __name__ == '__main__':
     rospy.init_node('gpg_lab3_node')
@@ -381,7 +319,6 @@ if __name__ == '__main__':
     global robotPosey
     global goalPosex
     global goalPosey
-    global hero
     global mapData 
     global mapWidth
     global mapList
@@ -400,34 +337,24 @@ if __name__ == '__main__':
     greenCells = GridCells()
     redCells = GridCells()
 
+
     rospy.sleep(1)
     subsSetup()
-    rospy.sleep(1)
-    #makeGraph2()
     rospy.sleep(0.5)
     while goalPosex == 0:
         pass
     rospy.sleep(1)
-    #makeGraph()
-   # aStar(mapList)
-    #rospy.sleep(1)
- 
-    #print mapList
-    mapList = makeGraph2()
-    #aStar2(mapList)
-    #for e in mapList:
-        #print "%f, %f" %(e.x, e.y)
-    print getNeighbors(Node(goalPosex, goalPosey, 100, 100), mapList)
     
-    createCell(4.2,4.2, "redCells")
-    rospy.sleep(0.3)
-    addCell(-3, -3, "redCells")
-    rospy.sleep(0.3)
+    mapList = makeGraph2()
+    finalPath = aStar2(mapList)
 
-    createCell(0, 0, "greenCells")
-    rospy.sleep(0.1)
-    addCell(1, 1, "greenCells")
-
+    print len(finalPath)
+    
+    createCell(robotPosex, robotPosey, "redCells")
+    for pathElement in finalPath:
+        addCell(pathElement.x,pathElement.y, "redCells")
+        rospy.sleep(0.1)
+            
 
 # sleep to allow initialization, spin waits for an event (btn press)
     time.sleep(1)
