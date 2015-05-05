142d68a5460525204f124bf902e299b985e260d0
wsbarnard <wsbarnard@wpi.edu>
2015-05-04 20:34:14 -0400

    Not entirely got it to work, it spins, mainly to get this over to Gabby's laptop

---
+++ final3002/helper.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import rospy, tf, time, math
+
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Pose2D
+
+
+def subscribeOriginal():
+    orig = rospy.Subscriber("cmd_vel", Twist, origCB)
+
+def origCB(data):
+    global speed
+    global angSpeed
+
+    speed = data.linear.x
+    angSpeed = data.angular.z
+
+
+
+
+#publishes a twist message t othe cmd_vel_mux/input/teleop topic
+def publishCopy():
+    global speed
+    global angSpeed
+    global pub1
+    pub1 = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size = 1)
+
+    twist = Twist() 
+    twist.linear.x = speed; twist.linear.y = 0; twist.linear.z = 0
+    twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = angSpeed
+    pub1.publish(twist)
+
+
+def publishOriginal():
+    global speed
+    global angSpeed
+    global pub2
+    pub2 = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+
+    twist = Twist()
+    twist.linear.x = speed; twist.linear.y = 0; twist.linear.z = 0
+    twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = angSpeed/4
+    pub2.publish(twist)
+
+if __name__ == '__main__':
+    #define globals
+    global speed
+    global angSpeed
+    global pub1
+    global pub2
+ 
+    subscribeOriginal()
+    rospy.init_node('wsbarnard_lab2_node')
+    
+    while(1):
+        publishOriginal()
+        publishCopy()
+        
+    rospy.spin()
+    
+
+
+    
+
+
+
